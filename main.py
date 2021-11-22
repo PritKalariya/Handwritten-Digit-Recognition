998202250286905d543617e82c8ac11bd112a903
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-11-22 23:44:37 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -16,17 +16,25 @@ mnist = tf.keras.datasets.mnist
 X_train = tf.keras.utils.normalize(X_train, axis=1)
 X_test = tf.keras.utils.normalize(X_test, axis=1)
 
-# Modeling
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+# # Modeling
+# model = tf.keras.models.Sequential()
+# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(128, activation='relu'))
+# model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
-# Complie
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# # Complie
+# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Fit the model
-model.fit(X_train, y_train, epochs=3)
+# # Fit the model
+# model.fit(X_train, y_train, epochs=3)
 
-model.save('Handwritten.model')
+# model.save('handwritten_digits.model')
+
+# Laod the model
+model = tf.keras.models.load_model('handwritten_digits.model')
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test, y_test)
+print(loss)
+print(accuracy)
