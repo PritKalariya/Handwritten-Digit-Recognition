b2b3b3a76c2d2c7ec7b6000dfd817c7893744aeb
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-11-22 23:58:57 +0530

    Testing the new model with custom images.

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ import tensorflow as tf
 
 
 # Load the dataset directly from the tf module
-mnist = tf.keras.datasets.mnist
+# mnist = tf.keras.datasets.mnist
 
-# Pre-Processing
-# Split train test
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# # Pre-Processing
+# # Split train test
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Normalising
-X_train = tf.keras.utils.normalize(X_train, axis=1)
-X_test = tf.keras.utils.normalize(X_test, axis=1)
+# # Normalising
+# X_train = tf.keras.utils.normalize(X_train, axis=1)
+# X_test = tf.keras.utils.normalize(X_test, axis=1)
 
 # # Modeling
 # model = tf.keras.models.Sequential()
@@ -34,7 +34,27 @@ X_test = tf.keras.utils.normalize(X_test, axis=1)
 # Laod the model
 model = tf.keras.models.load_model('handwritten_digits.model')
 
+
 # Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-print(loss)
-print(accuracy)
+# loss, accuracy = model.evaluate(X_test, y_test)
+# print(loss)
+# print(accuracy)
+
+
+# Testing
+# Load custom images and predict them
+image_number = 1
+
+while os.path.isfile(f'digits/digit{image_number}.png'):
+    try:
+        img = cv2.imread(f'digits/digit{image_number}.png')[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"The number is probably a {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+        image_number += 1
+    except:
+        print("Error reading image! Proceeding with next image...")
+    finally:
+        image_number += 1
